730e4d75ec1158357c2eac54dd7270ecaf2e3018
huangbinghui <hbh.personal@outlook.com>
2021-03-14 17:59:10 +0800

    hrRole

--- mapper/src/main/java/ltd/huangbinghui/vhr/mapper/HrMapper.java
+++ mapper/src/main/java/ltd/huangbinghui/vhr/mapper/HrMapper.java
@@ -1,4 +1,22 @@
 package ltd.huangbinghui.vhr.mapper;
 
+
+import ltd.huangbinghui.vhr.model.Hr;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
 public interface HrMapper {
+    @Select("select from hr where id = #{id}")
+    Hr findById(Integer id);
+    @Insert("insert into hr values(id,name,phone,telephone,address,enable,username,password,userface,remark)")
+    int insert(Hr record);
+
+    int insertSelective(Hr record);
+    @Delete("delete from hr where id = #{id}")
+    int deleteById(Integer id);
+    @Update("update he set name = #{name},phone = #{phone},telephone = #{telephone},address = #{address}," +
+            "enable = #{enable},username = #{username},password = #{password},userface = #{username},remark = #{remark}")
+    int updateById(Hr record);
+
+    int updateByIdSelective(Hr record);
 }
--- mapper/src/main/java/ltd/huangbinghui/vhr/mapper/HrRoleMapper.java
+++ mapper/src/main/java/ltd/huangbinghui/vhr/mapper/HrRoleMapper.java
@@ -1,4 +1,21 @@
 package ltd.huangbinghui.vhr.mapper;
 
+
+import ltd.huangbinghui.vhr.model.HrRole;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
 public interface HrRoleMapper {
+    @Select("select * from hrRole where id = #{id}")
+    HrRole findById(Integer id);
+    @Insert("insert into hrRole values (id,hrid,rid)")
+    int insert(HrRole record);
+
+    int insertSelective(HrRole record);
+    @Delete("delete from hrRole where id = #{id}")
+    int deleteById(Integer id);
+    @Update("update hrRole set hrid=#{hrid},rid=#{rid}")
+    int updateById(HrRole record);
+
+    int updateByIdSelective(HrRole record);
 }
